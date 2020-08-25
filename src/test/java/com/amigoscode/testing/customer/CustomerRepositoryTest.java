27b10f720cf41487b517be358c2018e319a6ac84
Dax Wann <daxwann@gmail.com>
2020-08-24 23:06:27 -0700

    add customer model, repository and test for selectCustomerByPhone

---
+++ src/test/java/com/amigoscode/testing/customer/CustomerRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.amigoscode.testing.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@DataJpaTest(properties = {"spring.jpa.properties.javax.persistence.validation.mode=none"})
+public class CustomerRepositoryTest {
+  private CustomerRepository testRepository;
+
+  @Autowired
+  public CustomerRepositoryTest(CustomerRepository testRepository) {
+    this.testRepository = testRepository;
+  }
+
+  @Test
+  void itShouldSelectCustomerByPhoneNumber() {
+    // given
+    UUID id = UUID.randomUUID();
+    String phoneNumber = "1111";
+    String name = "Hansel";
+    Customer customer = new Customer(id, name, phoneNumber);
+
+    testRepository.save(customer);
+
+    // test
+    Optional<Customer> optionalCustomer = testRepository.selectCustomerByPhoneNumber(phoneNumber);
+  }
+}
